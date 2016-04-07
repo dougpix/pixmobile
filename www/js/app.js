9c79f4c64e326a6ce6600fbcb41e1b68296f568f
Doug <doug@pixability.com>
2016-04-07 11:32:37 -0400

    now runs through jspm

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,13 @@
 // Ionic Starter App
 
+//import "ionic/js/dist/js/ionic";
+import "lib/ionic/js/ionic.bundle";
+import "angular";
+import "angular-animate";
+import "angular-ui-router";
+//import "ionic/js/dist/js/ionic-angular";
+
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
