95b8a54129427222b27eb087791742333032b6c7
Dani Dewitt <danidewitt@gmail.com>
2016-04-18 19:32:51 -0400

    fix graph render and filter issues

--- www/app/config/magicData.js
+++ www/app/config/magicData.js
@@ -1,8 +1,8 @@
 let magicData = { "adGroups":
     [
         {
-            "ad_group_name": "Target 1",
-            sm: 1,
+            "ad_group_name": "Boston Marathon",
+            "sm": 1,
             "vr": 22.6,
             "ctr": 3.5,
             "er": 2.0,
@@ -10,8 +10,8 @@ let magicData = { "adGroups":
             "cpv": 0.09
         },
         {
-            "ad_group_name": "Target 2",
-            sm: 2,
+            "ad_group_name": "March Madness",
+            "sm": 2,
             "vr": 22.6,
             "ctr": 4.4,
             "er": 9.2,
@@ -19,8 +19,8 @@ let magicData = { "adGroups":
             "cpv": 0.06
         },
         {
-            "ad_group_name": "Target 5",
-            sm: 3,
+            "ad_group_name": "Super Bowl 50",
+            "sm": 3,
             "vr": 22.6,
             "ctr": 2.3,
             "er": 16.5,
@@ -28,8 +28,8 @@ let magicData = { "adGroups":
             "cpv": 0.055
         },
         {
-            "ad_group_name": "Target 15",
-            sm: 4,
+            "ad_group_name": "Presidential Election",
+            "sm": 4,
             "vr": 22.6,
             "ctr": 1.7,
             "er": 35.6,
@@ -37,8 +37,8 @@ let magicData = { "adGroups":
             "cpv": 0.111
         },
         {
-            "ad_group_name": "Target 4",
-            sm: 5,
+            "ad_group_name": "SXSW",
+            "sm": 5,
             "vr": 22.6,
             "ctr": 4.7,
             "er": 34.0,
@@ -46,8 +46,8 @@ let magicData = { "adGroups":
             "cpv": 0.105
         },
         {
-            "ad_group_name": "Target 3",
-            sm: 6,
+            "ad_group_name": "Coachella",
+            "sm": 6,
             "vr": 22.6,
             "ctr": 4.4,
             "er": 45.9,
@@ -55,8 +55,8 @@ let magicData = { "adGroups":
             "cpv": 0.065
         },
         {
-            "ad_group_name": "Target 8",
-            sm: 7,
+            "ad_group_name": "Olympics",
+            "sm": 7,
             "vr": 22.6,
             "ctr": 4.4,
             "er": 45.9,
--- www/app/modules/winnerAnalysis/successGraph.directive.js
+++ www/app/modules/winnerAnalysis/successGraph.directive.js
@@ -8,29 +8,17 @@ function successGraphDirective () {
         link: function(scope, element, attrs) {
 
             let init = () => {
-                let margin = {top: 20, right: 20, bottom: 30, left: 40},
-                width = element[0].clientWidth - margin.left - margin.right,
-                height = 300 - margin.top - margin.bottom;
-                let x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-                let y = d3.scale.linear().range([height, 0]);
-                let xAxis = d3.svg.axis().scale(x).orient("bottom");
-                let yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
-                let color = d3.scale.ordinal().range(["#1E88E5", "#00BCD4", "#00C853"]);
-
-
-                scope.$watch('data', loadFreshGraphs);
 
                 let loadFreshGraphs = () => {
                     if (scope.data) {
                         addSuccessMetric(scope.data);
                     }
-                    //d3.json('.//source.json', function(json) {})
                 }
 
                 let buildSuccessGraph = (processedData) => {
-                    let rawData = processedData;
-                    let sortedData = _.orderBy(rawData, 'success_metric', 'desc')
-                    buildBarChart(sortedData, 'ad_group_name', 'success_metric', 'Ad Group', 'Ad Group Success');
+                    d3.selectAll("svg").remove();
+                    d3.selectAll(".d3-tip").remove();
+                    buildBarChart(processedData, 'ad_group_name', 'success_metric', 'Ad Group', 'Ad Group Success');
                 }
 
                 let addSuccessMetric = (data) => {
@@ -43,17 +31,26 @@ function successGraphDirective () {
                 let buildSuccesGraphTooltip = (d) => {
 
                     let toolTipString = "Ad Group: " + d.ad_group_name +
-                        "<br> Success Metric: " + d.success_metric +
-                        "<br> CPV: " + d.cpv +
-                        "<br> VR: " + d.vr +
-                        "<br> CTR: " + d.ctr +
-                        "<br> ER: " + d.er +
-                        "<br> VCR: " + d.vcr
+                        "<br> Success Metric: " + d.sm +
+                        "<br> Cost per view: " + d.cpv +
+                        "<br> View rate: " + d.vr +
+                        "<br> Click-through rate: " + d.ctr +
+                        "<br> Engagement rate: " + d.er +
+                        "<br> Video completion rate: " + d.vcr
 
                     return toolTipString;
                 }
 
                 let buildBarChart = (data, xUnit, yUnit, xAxisLabel, yAxisLabel, graphTitle) => {
+
+                    let margin = {top: 20, right: 20, bottom: 30, left: 40};
+                    let width = element[0].clientWidth - margin.left - margin.right;
+                    let height = 300 - margin.top - margin.bottom;
+                    let x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+                    let y = d3.scale.linear().range([height, 0]);
+                    let xAxis = d3.svg.axis().scale(x).orient("bottom");
+                    let yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+                    let color = d3.scale.ordinal().range(["#1E88E5", "#00BCD4", "#00C853"]);
 
                     var barChart = d3.select(element[0]).append("svg")
                         .attr("width", width + margin.left + margin.right)
@@ -128,6 +125,9 @@ function successGraphDirective () {
                 }
             })
 
+            scope.$watch('data', () => {
+                init();
+            });
 
         }
     }
